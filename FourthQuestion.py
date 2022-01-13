e1bbc952f9630c92765522bd3b6cf2f8b1e3a01b
zyadalazazi <zyadazazi@gmail.com>
2022-01-13 13:23:22 +0300

    first commit

---
+++ FourthQuestion.py
@@ -0,0 +1,9 @@
+
+# A function to add more than one number
+def add(*y):
+    return sum(y)
+
+
+# Calling the function
+print(add(2, 3))
+print(add(2, 3, 4))
---
+++ README.md
@@ -0,0 +1,1 @@
+# rowad-sixth-pychallenge
